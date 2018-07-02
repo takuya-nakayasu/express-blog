a1491e6822200b4bf76042b93082a6f55fc868c0
l08084 <l08084.1989@gmail.com>
2018-07-03 00:26:08 +0900

    show

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var sqlite3 = require('sqlite3');
+
+var db = new sqlite3.Database('mydb.sqlite3');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  db.serialize(() => {
+    db.all('select * from post', (err, rows) => {
+      if (!err) {
+        res.render('index', { posts: rows });
+      }
+    });
+  });
 });
 
 module.exports = router;
