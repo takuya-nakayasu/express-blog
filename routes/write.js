0e038ade68363fac6860a6bc0ecf97e93190cbd1
l08084 <l08084.1989@gmail.com>
2018-07-02 23:40:06 +0900

    add write

--- routes/write.js
+++ routes/write.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var sqlite3 = require('sqlite3');
 
-router.get('/', function(req, res, next) {
-    res.render('write');
+var db = new sqlite3.Database('mydb.sqlite3');
+
+router.get('/', (req, res, next) => res.render('write'));
+
+router.post('/', (req, res, next) => {
+    console.log('dbb');
+    console.log(db);
+    const title = req.body.title;
+    const content = req.body.content;
+    const createdtime = new Date();
+    db.run(
+        'insert into post (title, content, createdtime) values (?, ?, ?)',
+        title,
+        content,
+        createdtime
+    );
+    res.redirect('/');
 });
-  
+
 module.exports = router;
